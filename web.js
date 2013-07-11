3d796099fa79333ab55ba132232467f59d15b278
epocolis <leotisbuchanan@gmail.com>
2013-07-11 10:17:22 -0400

    Reading data from index file

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var d = ""; 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+    fs.readFileSync('index.html', function (data) {
+	response.send(data);
+    });
+
+    
+
 });
 
 var port = process.env.PORT || 5000;
